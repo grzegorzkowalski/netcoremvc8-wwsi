ee0d9c1f70476e53eec9a96e4fd236a4e1096f6a
Grzegorz Kowalski <grzegorzkowalski@yahoo.com>
2024-05-12 12:39:42 +0200

    TravelBlog new project

---
+++ 5-7_mvc_i_EFCore/TravelBlog/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TravelBlog.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
